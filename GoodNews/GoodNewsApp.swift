4b2615586b268b19f1d829b70a0a5cb7afed46af
Lisandro Falconi <lfalconi.consultant@newscorp.com>
2020-09-02 18:39:13 -0300

    Initial Commit

---
+++ GoodNews/GoodNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodNewsApp.swift
+//  GoodNews
+//
+//  Created by Lisandro on 02/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct GoodNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
